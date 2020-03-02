559ce519dbccbc691ad214f467d8f9c8bf4b93a9
loopyme <peter@mail.loopy.tech>
2020-03-02 11:46:05 +0800

    fix a bug

--- cquai_ml/KNeighborsClassifier.py
+++ cquai_ml/KNeighborsClassifier.py
@@ -3,15 +3,15 @@ from sklearn.neighbors import BallTree, KDTree
 
 
 class KNeighborsClassifier:
-    """ 一个仿scikit-learn接口的微型knn实现。
+    """ A KNN implementation imitating scikit-learn interface.
     Ｄependency
     ----------
-    `scipy` : 考虑到性能，使用scipy来完成底层运算
+    `scipy` : For performance reasons, use scipy to perform the underlying operations
         - `scipy.unique`
         - `scipy.spatial.distance.cdist`
         - `scipy.argsort`
         - `scipy.stats.mode`
-    `sklearn.neighbors` ： 由于时间有限暂时还没有实现的数据结构(TODO: implement of BallTree & KDTree)
+    `sklearn.neighbors` ： Data structures that have not yet been implemented (TODO: implement of BallTree & KDTree)
         - `sklearn.neighbors.BallTree`
         - `sklearn.neighbors.KDTree`
     Parameters
@@ -48,35 +48,32 @@ class KNeighborsClassifier:
     [[0.66666667 0.33333333]]
     """
 
-    class KNeighborsClassifier:
-        def __init__(self, n_neighbors=5, algorithm="brute", leaf_size=30):
-            self.n_neighbors = n_neighbors
-            self.algorithm = algorithm
-            self.leaf_size = leaf_size
+    def __init__(self, n_neighbors=5, algorithm="brute", leaf_size=30):
+        self.n_neighbors = n_neighbors
+        self.algorithm = algorithm
+        self.leaf_size = leaf_size
 
-        def fit(self, X, y):
-            self._fit_X = X
-            self.classes_, self._fit_y = scipy.unique(y, return_inverse=True)
+    def fit(self, X, y):
+        self._fit_X = X
+        self.classes_, self._fit_y = scipy.unique(y, return_inverse=True)
 
-            if self.algorithm == "brute":
-                pass
-            elif self.algorithm == "kd_tree":
-                self.tree = KDTree(X, leaf_size=self.leaf_size)
-            elif self.algorithm == "ball_tree":
-                self.tree = BallTree(X, leaf_size=self.leaf_size)
-            else:
-                raise ValueError("unrecognized algorithm: ", str(self.algorithm))
-            return self
+        if self.algorithm == "brute":
+            pass
+        elif self.algorithm == "kd_tree":
+            self.tree = KDTree(X, leaf_size=self.leaf_size)
+        elif self.algorithm == "ball_tree":
+            self.tree = BallTree(X, leaf_size=self.leaf_size)
+        else:
+            raise ValueError("unrecognized algorithm: ", str(self.algorithm))
+        return self
 
-        def predict(self, X):
-            if self.algorithm == "brute":
-                dist_mat = scipy.spatial.distance.cdist(X, self._fit_X)
-                neighbors_ind = scipy.argsort(dist_mat, axis=1)[:, : self.n_neighbors]
-            elif self.algorithm in ["kd_tree", "ball_tree"]:
-                dist_mat, neighbors_ind = self.tree.query(X, k=self.n_neighbors)
-            else:
-                raise ValueError("unrecognized algorithm: ", str(self.algorithm))
-            neighbors_y = scipy.stats.mode(self._fit_y[neighbors_ind], axis=1)[
-                0
-            ].flatten()
-            return self.classes_[neighbors_y]
+    def predict(self, X):
+        if self.algorithm == "brute":
+            dist_mat = scipy.spatial.distance.cdist(X, self._fit_X)
+            neighbors_ind = scipy.argsort(dist_mat, axis=1)[:, : self.n_neighbors]
+        elif self.algorithm in ["kd_tree", "ball_tree"]:
+            dist_mat, neighbors_ind = self.tree.query(X, k=self.n_neighbors)
+        else:
+            raise ValueError("unrecognized algorithm: ", str(self.algorithm))
+        neighbors_y = scipy.stats.mode(self._fit_y[neighbors_ind], axis=1)[0].flatten()
+        return self.classes_[neighbors_y]
